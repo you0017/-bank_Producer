f7eb8de6af89b17260b053427437f25b20ddc7c3
you0017 <2092425375@qq.com>
2024-08-20 18:32:15 +0800

    springboot3_redis_springCache_jms_email_mybatisPlus

--- src/main/java/com/yc/bean/MessageBean.java
+++ src/main/java/com/yc/bean/MessageBean.java
@@ -12,7 +12,7 @@ import lombok.ToString;
 public class MessageBean {
     private Accounts account;
     private double money;
-    private Integer toAccountid;
+    private Integer toAccountId;
     private String email;
     private String opType;
 }
